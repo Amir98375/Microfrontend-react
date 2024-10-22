3bd5bdea2665f19a551da998013ba03662d0b391
Mohd Amir <Amirkhan98375@gmail.com>
2024-10-22 22:48:55 +0530

    todo  using microfrontend

---
+++ todo-components/src/components/List.jsx
@@ -0,0 +1,13 @@
+
+const List = (props) => {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
